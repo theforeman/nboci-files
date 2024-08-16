6fa4b90c1849c3bb80ebda6b93b11335176ab083
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-08-16 13:06:15 +0200

    Use random directories while building containers
    This creates a guaranteed fresh directory for each step, rather than
    reusing a directory multiple times. This can make a difference when
    hidden files are present.

--- push-fedora-40.sh
+++ push-fedora-40.sh
@@ -2,14 +2,16 @@
 
 OS=fedora
 OSVER=fedora-40
+WORKDIR=$PWD/work
 
 # create image index and set the annotation at the index level
 # TODO set the artifactType at the index level https://github.com/containers/podman/issues/23598
 podman manifest create quay.io/foreman/nboci-files:$OSVER --annotation org.pulpproject.netboot.version=1
 
-mkdir -p work
-rm -f ./work/*
-podman build -v $(pwd)/work:/root:Z -f Containerfile-$OS-amd64 --platform linux/amd64
+mkdir -p "$WORKDIR"
+VOLUME1=$(mktemp -d -p "$WORKDIR")
+
+podman build -v "${VOLUME1}:/root:Z" -f Containerfile-$OS-amd64 --platform linux/amd64
 
 # create and add netboot artifacts for amd64 to the image index
 podman manifest add --annotation org.pulpproject.netboot.version=1 \
@@ -17,10 +19,10 @@ podman manifest add --annotation org.pulpproject.netboot.version=1 \
     --artifact \
     --artifact-type application/vnd.org.pulpproject.netboot.artifact.v1 \
     --artifact-layer-type application/x-netboot-file \
-    quay.io/foreman/nboci-files:$OSVER ./work/*
+    quay.io/foreman/nboci-files:$OSVER "$VOLUME1"/*
 
-rm -f ./work/*
-podman build -v $(pwd)/work:/root:Z -f Containerfile-$OS-arm64 --platform linux/arm64
+VOLUME2=$(mktemp -d -p "$WORKDIR")
+podman build -v "${VOLUME2}:/root:Z" -f Containerfile-$OS-arm64 --platform linux/arm64
 
 # create and add netboot artifacts for arm64 to the image index
 podman manifest add --annotation org.pulpproject.netboot.version=1 \
@@ -28,9 +30,10 @@ podman manifest add --annotation org.pulpproject.netboot.version=1 \
     --artifact \
     --artifact-type application/vnd.org.pulpproject.netboot.artifact.v1 \
     --artifact-layer-type application/x-netboot-file \
-    quay.io/foreman/nboci-files:$OSVER ./work/*
+    quay.io/foreman/nboci-files:$OSVER "$VOLUME2"/*
 
-rm -rf ./work/
+rm -rf "$WORKDIR"
+
 # push image index containing netboot artifacts for different arches
 podman manifest push --all --rm quay.io/foreman/nboci-files:$OSVER
 
