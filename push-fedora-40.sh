theforeman/nboci-files

commit b8ed17a43196b26af61a8dd45aef41a904d74b9d

Update the push-fedora-40 script so it properly creates one image index with child manifests.
[noissue]

* adds proper artifactType
* adds proper artifact layer type
* child per-arches manifests are tagged

push-fedora-40.sh
@@ -3,28 +3,38 @@
 OS=fedora
 OSVER=fedora-40
 
-podman manifest create quay.io/foreman/nboci-files:$OSVER
+# create image index and set the annotation at the index level
+# TODO set the artifactType at the index level https://github.com/containers/podman/issues/23598
+podman manifest create quay.io/foreman/nboci-files:$OSVER --annotation org.pulpproject.netboot.version=1
 
+mkdir -p work
 rm -f ./work/*
 podman build -v $(pwd)/work:/root:Z -f Containerfile-$OS-amd64 --platform linux/amd64
 
-podman manifest create quay.io/foreman/nboci-files:$OSVER-amd64
+# create and add netboot artifacts for amd64 to the image index
 podman manifest add --annotation org.pulpproject.netboot.version=1 \
     --os linux --arch amd64 --os-version $OSVER \
-    --artifact quay.io/foreman/nboci-files:$OSVER-amd64 ./work/*
-podman manifest push quay.io/foreman/nboci-files:$OSVER-amd64
+    --artifact \
+    --artifact-type application/vnd.org.pulpproject.netboot.artifact.v1 \
+    --artifact-layer-type application/x-netboot-file \
+    quay.io/foreman/nboci-files:$OSVER-amd64 ./work/*
 
 rm -f ./work/*
 podman build -v $(pwd)/work:/root:Z -f Containerfile-$OS-arm64 --platform linux/arm64
 
-podman manifest create quay.io/foreman/nboci-files:$OSVER-arm64
+# create and add netboot artifacts for arm64 to the image index
 podman manifest add --annotation org.pulpproject.netboot.version=1 \
     --os linux --arch arm64 --os-version $OSVER \
-    --artifact quay.io/foreman/nboci-files:$OSVER-arm64 ./work/*
-podman manifest push quay.io/foreman/nboci-files:$OSVER-arm64
+    --artifact \
+    --artifact-type application/vnd.org.pulpproject.netboot.artifact.v1 \
+    --artifact-layer-type application/x-netboot-file \
+    quay.io/foreman/nboci-files:$OSVER-arm64 ./work/*
 
-podman manifest add --all --annotation org.pulpproject.netboot.version=1 quay.io/foreman/nboci-files:$OSVER \
-    quay.io/foreman/nboci-files:$OSVER-amd64 \
-    quay.io/foreman/nboci-files:$OSVER-arm64
+rm -rf ./work/
+# push image index containing netboot artifacts for different arches
+podman manifest push --all --rm quay.io/foreman/nboci-files:$OSVER
 
-podman manifest push --all quay.io/foreman/nboci-files:$OSVER
+# tag child manifests
+# going to use skopeo for now https://github.com/containers/podman/issues/23599
+# Parse index image, find corresponding child manifests and tag them accordingly
+skopeo inspect docker://quay.io/foreman/nboci-files:$OSVER --raw |jq -r '.manifests.[] | "\(.platform.architecture) \(.digest)"' | while read i; do ARCH="$(echo $i | awk '{print $1}')"; DIGEST="$(echo $i | awk '{print $2}')" ; skopeo copy docker://quay.io/foreman/nboci-files@${DIGEST} docker://quay.io/foreman/nboci-files:$OSVER-${ARCH} ; done

commit aaa0b209caaca75d40baa6e842dfcbb57c56a716

Refactor the script to be generic (#2)
* Remove arch from container tags

Fixes: b8ed17a43196 ("Update the push-fedora-40 script so it properly creates one image index with child manifests.")

* Use random directories while building containers

This creates a guaranteed fresh directory for each step, rather than
reusing a directory multiple times. This can make a difference when
hidden files are present.

* Add quotes to avoid potential splitting and use read -r

These were both flagged by shellcheck.

* Introduce a repository variable

* Dynamically determine architectures based on Containerfiles

This globs over containerfiles and assumes they follow the
Containerfile-$OS-$ARCH naming pattern.

* Read OS and VERSION from environment variables

This makes it easy to build a container for a different OS or version.
It still has the same defaults, but it's renamed because it's now a
generic script.

* Optimize Fedora container files

This uses /etc/os-release to get some values that are OS-dependent which
makes it easy to see the same thing is going on when we do the same for
other distributions.

It also uses a single curl command which allows for connection reuse.
Long options are used for readability.

* Add CentOS Containerfile definitions

* Remove unused name argument from Containerfile

* Change WORKDIR to /work

This avoids the overlap between root's home directory and the working
directory.

* fixup! Read OS and VERSION from environment variables

Containerfile-centos-amd64
@@ -1,0 +1,16 @@
+ARG version=stream9
+
+FROM quay.io/centos/centos:$version-minimal as builder
+ARG arch=x64
+ARG baseurl=https://mirror.stream.centos.org
+WORKDIR /work
+
+# Artifacts from kickstart repository.
+RUN . /etc/os-release && curl --location --remote-name-all --remote-time \
+	${baseurl}/${VERSION}-stream/BaseOS/$(uname -m)/os/images/pxeboot/vmlinuz \
+	${baseurl}/${VERSION}-stream/BaseOS/$(uname -m)/os/images/pxeboot/initrd.img \
+	${baseurl}/${VERSION}-stream/BaseOS/$(uname -m)/os/images/install.img
+
+# Artifacts from RPM repository.
+RUN microdnf -y install shim-${arch} grub2-efi-${arch} syslinux-tftpboot
+RUN . /etc/os-release && cp -p /tftpboot/pxelinux.0 . && cp -p /boot/efi/EFI/${ID}/{shim,grub}${arch}.efi .

Containerfile-centos-arm64
@@ -1,0 +1,16 @@
+ARG version=stream9
+
+FROM quay.io/centos/centos:$version-minimal as builder
+ARG arch=aa64
+ARG baseurl=https://mirror.stream.centos.org
+WORKDIR /work
+
+# Artifacts from kickstart repository.
+RUN . /etc/os-release && curl --location --remote-name-all --remote-time \
+	${baseurl}/${VERSION}-stream/BaseOS/$(uname -m)/os/images/pxeboot/vmlinuz \
+	${baseurl}/${VERSION}-stream/BaseOS/$(uname -m)/os/images/pxeboot/initrd.img \
+	${baseurl}/${VERSION}-stream/BaseOS/$(uname -m)/os/images/install.img
+
+# Artifacts from RPM repository.
+RUN microdnf -y install shim-${arch} grub2-efi-${arch} syslinux-tftpboot
+RUN . /etc/os-release && cp -p /tftpboot/pxelinux.0 . && cp -p /boot/efi/EFI/${ID}/{shim,grub}${arch}.efi .

Containerfile-fedora-amd64
@@ -1,15 +1,16 @@
-FROM quay.io/fedora/fedora-minimal:40 as builder
-ARG name=Fedora
 ARG version=40
+
+FROM quay.io/fedora/fedora-minimal:$version as builder
 ARG arch=x64
 ARG baseurl=https://dl.fedoraproject.org/pub/fedora/linux
-WORKDIR /root
+WORKDIR /work
 
 # Artifacts from kickstart repository.
-RUN curl -RLO ${baseurl}/releases/${version}/Everything/$(uname -m)/os/images/pxeboot/vmlinuz
-RUN curl -RLO ${baseurl}/releases/${version}/Everything/$(uname -m)/os/images/pxeboot/initrd.img
-RUN curl -RLO ${baseurl}/releases/${version}/Everything/$(uname -m)/os/images/install.img
+RUN . /etc/os-release && curl --location --remote-name-all --remote-time \
+	${baseurl}/releases/${VERSION}/Everything/$(uname -m)/os/images/pxeboot/vmlinuz \
+	${baseurl}/releases/${VERSION}/Everything/$(uname -m)/os/images/pxeboot/initrd.img \
+	${baseurl}/releases/${VERSION}/Everything/$(uname -m)/os/images/install.img
 
 # Artifacts from RPM repository.
 RUN microdnf -y install shim-${arch} grub2-efi-${arch} syslinux-tftpboot
-RUN cp -p /tftpboot/pxelinux.0 . && cp -p /boot/efi/EFI/fedora/{shim,grub${arch}}.efi .
+RUN . /etc/os-release && cp -p /tftpboot/pxelinux.0 . && cp -p /boot/efi/EFI/${ID}/{shim,grub}${arch}.efi .

Containerfile-fedora-arm64
@@ -1,15 +1,16 @@
-FROM quay.io/fedora/fedora-minimal:40 as builder
-ARG name=Fedora
 ARG version=40
+
+FROM quay.io/fedora/fedora-minimal:$version as builder
 ARG arch=aa64
 ARG baseurl=https://dl.fedoraproject.org/pub/fedora/linux
-WORKDIR /root
+WORKDIR /work
 
 # Artifacts from kickstart repository.
-RUN curl -RLO ${baseurl}/releases/${version}/Everything/$(uname -m)/os/images/pxeboot/vmlinuz
-RUN curl -RLO ${baseurl}/releases/${version}/Everything/$(uname -m)/os/images/pxeboot/initrd.img
-RUN curl -RLO ${baseurl}/releases/${version}/Everything/$(uname -m)/os/images/install.img
+RUN . /etc/os-release && curl --location --remote-name-all --remote-time \
+	${baseurl}/releases/${VERSION}/Everything/$(uname -m)/os/images/pxeboot/vmlinuz \
+	${baseurl}/releases/${VERSION}/Everything/$(uname -m)/os/images/pxeboot/initrd.img \
+	${baseurl}/releases/${VERSION}/Everything/$(uname -m)/os/images/install.img
 
 # Artifacts from RPM repository.
 RUN microdnf -y install shim-${arch} grub2-efi-${arch}
-RUN cp -p /boot/efi/EFI/fedora/{shim,grub${arch}}.efi .
+RUN . /etc/os-release && cp -p /boot/efi/EFI/${ID}/{shim,grub}${arch}.efi .

push
@@ -1,0 +1,40 @@
+#!/bin/bash
+
+shopt nullglob
+
+OS=${OS:-fedora}
+VERSION=${VERSION:-40}
+OSVER=$OS-$VERSION
+WORKDIR=$PWD/work
+REPOSITORY=${REPOSITORY:-quay.io/foreman/nboci-files}
+TAG=$REPOSITORY-$OSVER
+
+# create image index and set the annotation at the index level
+# TODO set the artifactType at the index level https://github.com/containers/podman/issues/23598
+podman manifest create "$TAG" --annotation org.pulpproject.netboot.version=1
+
+mkdir -p "$WORKDIR"
+
+for containerfile in "Containerfile-$OS"-* ; do
+	ARCH=${containerfile##*-}
+	VOLUME=$(mktemp -d -p "${WORKDIR}")
+	podman build -v "${VOLUME}:/work:Z" -f "$containerfile" --platform "linux/$ARCH" --build-arg "version=$VERSION"
+
+	# create and add netboot artifacts to the image index
+	podman manifest add --annotation org.pulpproject.netboot.version=1 \
+	    --os linux --arch "$ARCH" --os-version "$OSVER" \
+	    --artifact \
+	    --artifact-type application/vnd.org.pulpproject.netboot.artifact.v1 \
+	    --artifact-layer-type application/x-netboot-file \
+	    "$TAG" "$VOLUME"/*
+done
+
+rm -rf "$WORKDIR"
+
+# push image index containing netboot artifacts for different arches
+podman manifest push --all --rm "$TAG"
+
+# tag child manifests
+# going to use skopeo for now https://github.com/containers/podman/issues/23599
+# Parse index image, find corresponding child manifests and tag them accordingly
+skopeo inspect "docker://$TAG" --raw |jq -r '.manifests.[] | "\(.platform.architecture) \(.digest)"' | while read -r i; do ARCH="$(echo "$i" | awk '{print $1}')"; DIGEST="$(echo "$i" | awk '{print $2}')" ; skopeo copy "docker://$REPOSITORY@${DIGEST}" "docker://$TAG-${ARCH}" ; done

push-fedora-40.sh
@@ -1,40 +1,0 @@
-#!/bin/bash
-
-OS=fedora
-OSVER=fedora-40
-
-# create image index and set the annotation at the index level
-# TODO set the artifactType at the index level https://github.com/containers/podman/issues/23598
-podman manifest create quay.io/foreman/nboci-files:$OSVER --annotation org.pulpproject.netboot.version=1
-
-mkdir -p work
-rm -f ./work/*
-podman build -v $(pwd)/work:/root:Z -f Containerfile-$OS-amd64 --platform linux/amd64
-
-# create and add netboot artifacts for amd64 to the image index
-podman manifest add --annotation org.pulpproject.netboot.version=1 \
-    --os linux --arch amd64 --os-version $OSVER \
-    --artifact \
-    --artifact-type application/vnd.org.pulpproject.netboot.artifact.v1 \
-    --artifact-layer-type application/x-netboot-file \
-    quay.io/foreman/nboci-files:$OSVER-amd64 ./work/*
-
-rm -f ./work/*
-podman build -v $(pwd)/work:/root:Z -f Containerfile-$OS-arm64 --platform linux/arm64
-
-# create and add netboot artifacts for arm64 to the image index
-podman manifest add --annotation org.pulpproject.netboot.version=1 \
-    --os linux --arch arm64 --os-version $OSVER \
-    --artifact \
-    --artifact-type application/vnd.org.pulpproject.netboot.artifact.v1 \
-    --artifact-layer-type application/x-netboot-file \
-    quay.io/foreman/nboci-files:$OSVER-arm64 ./work/*
-
-rm -rf ./work/
-# push image index containing netboot artifacts for different arches
-podman manifest push --all --rm quay.io/foreman/nboci-files:$OSVER
-
-# tag child manifests
-# going to use skopeo for now https://github.com/containers/podman/issues/23599
-# Parse index image, find corresponding child manifests and tag them accordingly
-skopeo inspect docker://quay.io/foreman/nboci-files:$OSVER --raw |jq -r '.manifests.[] | "\(.platform.architecture) \(.digest)"' | while read i; do ARCH="$(echo $i | awk '{print $1}')"; DIGEST="$(echo $i | awk '{print $2}')" ; skopeo copy docker://quay.io/foreman/nboci-files@${DIGEST} docker://quay.io/foreman/nboci-files:$OSVER-${ARCH} ; done